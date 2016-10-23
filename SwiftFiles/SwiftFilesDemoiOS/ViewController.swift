6decac22243398da36b6edfd59367767c3cd9f05
Jason Mercer <jasemercer@gmail.com>
2016-10-23 14:07:36 -0400

    Fixed a bug in iOS demo app

--- SwiftFiles/SwiftFilesDemoiOS/ViewController.swift
+++ SwiftFiles/SwiftFilesDemoiOS/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let files = FileList.allFilesAndFolders(
     }
 
     override func didReceiveMemoryWarning() {
